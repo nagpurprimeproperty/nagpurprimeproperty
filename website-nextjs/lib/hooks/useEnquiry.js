49523e0322c791b71cc6d4a4f49f37e41dc265bd
Tichuu77 <akashtikhat50@gmail.com>
2026-08-23 00:40:54 +0530

    teat CI/CD

--- website-nextjs/lib/hooks/useEnquiry.js
+++ website-nextjs/lib/hooks/useEnquiry.js
@@ -2,7 +2,7 @@
 import { useMutation } from '@tanstack/react-query';
 import { clientFetch } from '@/lib/fetcher';
 
-export function useSubmitEnquiry() { 
+export function useSubmitEnquiry() {
   return useMutation({
     mutationFn: ({ propertyId, data, token }) =>
       clientFetch(`/api/properties/${propertyId}/enquiry`, {
