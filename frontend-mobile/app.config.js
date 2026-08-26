50fe8b290e21a929ad47238d9ca5649bcfb8f70e
ayushbharne19 <ayush.bharne@talentrisetechnokrate.online>
2026-08-26 22:06:40 +0530

    fix: update app version to 1.0.3 and adjust onboarding layout styles

--- frontend-mobile/app.config.js
+++ frontend-mobile/app.config.js
@@ -35,7 +35,7 @@ module.exports = ({ config }) => {
     ...config,
     name: "Nagpur Prime Property",
     slug: "nagpur-prime-property",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/images/applogo.png",
     scheme: "nagpurprimeproperty",
